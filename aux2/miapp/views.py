ff51047cc16c93f845b4ded730a7bbd9b353dadf
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-08-13 19:40:36 -0400

    linked template files to miapp

--- aux2/miapp/views.py
+++ aux2/miapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def cuerpo_docente(request):
+    return render(request, 'miapp/cuerpo_docente.html')
+
+
+def index(request):
+    return render(request, 'miapp/index.html')
+
+
+def pestaña(request):
+    return render(request, 'miapp/pestaña.html')
